94e664ffb44b6455d8027415e5816bf974ca2fa9
cheng <cheng@localhost.localdomain>
2020-02-23 00:02:46 +0800

    fileshare pyqrcode terminal

---
+++ local_packages/fileshare.py
@@ -0,0 +1,119 @@
+import os, sys
+import subprocess
+import platform
+import socket
+import pyqrcode
+
+
+def print_file(fileDir):
+    OS = platform.system()
+    if OS == 'Darwin':
+        subprocess.call(['open', fileDir])
+    elif OS == 'Linux':
+        subprocess.call(['xdg-open', fileDir])
+    else:
+        os.startfile(fileDir)
+
+
+def get_qr(data, img_file=''):
+    img_file = img_file or 'QR.png'
+    qr = pyqrcode.create(data)
+    qr.png(img_file, 10)
+    print_file(img_file)
+
+
+def print_cmd_qr(qrText, white='', black='  ', enableCmdQR=True):
+    if not white:
+        try:
+            b = u'\u2588'
+            sys.stdout.write(b + '\r')
+            sys.stdout.flush()
+        except UnicodeEncodeError:
+            BLOCK = 'MM'
+        else:
+            BLOCK = b
+        white = BLOCK
+    blockCount = int(enableCmdQR)
+    if abs(blockCount) == 0:
+        blockCount = 1
+    white *= abs(blockCount)
+    if blockCount < 0:
+        white, black = black, white
+    sys.stdout.write(' '*50 + '\r')
+    sys.stdout.flush()
+    qr = qrText.replace('0', white).replace('1', black)
+    sys.stdout.write(qr)
+    sys.stdout.flush()
+
+
+def get_ipv4(dns=None):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect((dns or "8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    except OSError:
+        ip = None
+    finally:
+        s.close()
+    return ip
+
+
+def get_ipv6(dns=None):
+    try:
+        s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+        s.connect((dns or "2001:4860:4860::8888", 80))
+        ip = s.getsockname()[0]
+    except OSError:
+        ip = None
+    finally:
+        s.close()
+    return ip
+
+
+if __name__ == '__main__':
+    import argparse
+    from functools import partial
+    from http import server
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cgi', action='store_true',
+                       help='Run as CGI Server')
+    parser.add_argument('--bind', '-b', default='', metavar='ADDRESS',
+                        help='Specify alternate bind address '
+                             '[default: all interfaces]')
+    parser.add_argument('--directory', '-d', default=os.getcwd(),
+                        help='Specify alternative directory '
+                        '[default:current directory]')
+    parser.add_argument('port', action='store',
+                        default=8000, type=int,
+                        nargs='?',
+                        help='Specify alternate port [default: 8000]')
+    parser.add_argument('--enableCmdQR', '-s', type=int, default=0)
+    args = parser.parse_args()
+    if args.cgi:
+        handler_class = server.CGIHTTPRequestHandler
+    else:
+        handler_class = partial(server.SimpleHTTPRequestHandler,
+                                directory=args.directory)
+    ServerClass = server.ThreadingHTTPServer
+    if args.bind == "::":
+        ServerClass.address_family = socket.AF_INET6
+        ipaddr = "http://[{}]:{}".format(get_ipv6(), args.port)
+    else:
+        ip = get_ipv4()
+        ipaddr = "http://{}:{}".format(ip, args.port)
+    img_file = ".QR.png"
+    if args.enableCmdQR:
+        qr = pyqrcode.create(ipaddr)
+        print_cmd_qr(qr.text(1), enableCmdQR=args.enableCmdQR)
+        print(qr.terminal())
+    else:
+        get_qr(data=ipaddr, img_file=img_file)
+    print(ipaddr)
+    server.test(
+        HandlerClass=handler_class,
+        ServerClass=ServerClass,
+        port=args.port, bind=args.bind
+    )
+    # os.remove(img_file)
+
---
+++ local_packages/mail.py
@@ -0,0 +1,63 @@
+#!/usr/lib/env python3
+import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
+from collections import Iterable
+
+
+class EmailHelper:
+    def __init__(self, **kwargs):
+        self.host = kwargs.get("host") or "smtp.163.com"  # SMTP服务器
+        self.user = kwargs.get("user") or "leanote_phichem@163.com"  # 用户名
+        self.sender = kwargs.get("sender") or "leanote_phichem@163.com"  # 发件人邮箱
+        self.password = kwargs.get("password")  # 邮箱客户端授权码
+        if not self.password:
+            from .config import mail as _mail
+            self.password = _mail["password"]
+        self.receivers = []
+        __receivers = kwargs.get("receivers")
+        if not __receivers:
+            self.receivers = ['277199942@qq.com']
+        elif isinstance(__receivers, str):
+            self.receivers.append(__receivers)
+        elif Iterable(__receivers):
+            self.receivers.extend(__receivers)
+
+    def sendmail(self, title, content, **kwargs):
+        message = MIMEText(content, 'plain', 'utf-8')
+        message['From'] = "{}".format(self.sender)
+        receivers = kwargs.get("receivers") or self.receivers
+        message['To'] = ",".join(receivers)
+        message['Subject'] = title
+        try:
+            smtpObj = smtplib.SMTP_SSL(self.host, 465)  # 启用SSL发信, 端口一般是465
+            smtpObj.login(self.user, self.password)  # 登录验证
+            smtpObj.sendmail(self.sender, receivers, message.as_string())  # 发送
+            print("mail has been send successfully.")
+        except smtplib.SMTPException as e:
+            print(e)
+
+    def send():
+        pass
+
+# def send_email2(SMTP_host, from_account, from_passwd, to_account, subject, content):
+#     email_client = smtplib.SMTP(SMTP_host)
+#     email_client.login(from_account, from_passwd)
+#     # create msg
+#     msg = MIMEText(content, 'plain', 'utf-8')
+#     msg['Subject'] = Header(subject, 'utf-8')  # subject
+#     msg['From'] = from_account
+#     msg['To'] = to_account
+#     email_client.sendmail(from_account, to_account, msg.as_string())
+#
+#     email_client.quit()
+
+
+if __name__ == '__main__':
+    import time
+    mail = EmailHelper()
+    mail.sendmail(
+        "会议时间", "2020-02-14 12:20:00,\n当前: {}".format(
+            time.strftime("%Y-%m-%d %H:%M:%S")
+        )
+    )

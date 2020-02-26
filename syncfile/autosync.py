32cd580b07ee9950bd7084567718f3f09fe3faf7
shenchucheng/hs <shenchucheng@126.com>
2020-02-26 23:48:59 +0800

    bypy syncup with argparse

--- syncfile/autosync.py
+++ syncfile/autosync.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os
 import yaml
 from bypy import ByPy
@@ -7,25 +8,39 @@ pan = ByPy()
 __home = os.path.expanduser("~")
 __default = ".TransferStation/"
 __conffile = os.path.join(__home, ".bypy", "sync.conf")
-if not os.path.exists(__conffile):
-    with open(__conffile, "w") as f:
-        f.write("# This is used for bypy to syncup file\n")
-        f.write("# Setting the local and remote path \n")
-        f.write("# Example:\n# C:\\User\\cheng\\Downloads: test/downloads")
-        f.write("# C:\\User\\cheng\\.bypy: /bypy/app/test/.bypy")
 
 
-def sync_file():
-    with open(__conffile) as f:
-        conf = yaml.load(f, Loader=yaml.FullLoader)
+def sync_file(conf, default):
+    if not os.path.exists(conf):
+        with open(conf, "w") as f:
+            f.write("# This is used for bypy to syncup file\n")
+            f.write("# Setting the local and remote path \n")
+            f.write("# Example:\n# C:\\User\\cheng\\Downloads: test/downloads")
+            f.write("# /home/cheng/.bypy: test/.bypy")
+
+    with open(conf) as f:
+        __conf = yaml.load(f, Loader=yaml.FullLoader)
         if conf is None:
             raise NotImplementedError(
                 "Fatal Error: syncup conf file without setting. " +
                 "File has initialised in {}".format(__conffile)
             )
-    for l, r in conf.items():
+
+    for l, r in __conf.items():
         pan.syncup(l, r or __default+l.replace(os.sep, '.').replace(":", ''))
 
 
 if __name__ == '__main__':
-    sync_file()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--remote', '-r', default=__default,
+        help='syncup file to remote dir if not set, default: {}'.format(__default)
+    )
+    parser.add_argument(
+        '--config', '-c', default=__conffile, 
+        help='config file path, default: {}'.format(__conffile)
+    )
+    args = parser.parse_args()
+    sync_file(args.config, args.remote)
+
